b095097c713211dae9ac2b303fa86b9af4f69919
Harry Holmes <harryholmesse@gmail.com>
2024-12-06 11:49:27 +0100

    Fixed issue for file not saving or loading

--- MainApp/Services/ContactService.cs
+++ MainApp/Services/ContactService.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Models;
+﻿using Data.Services;
+using Domain.Models;
 using MainApp.Interfaces;
 
 namespace MainApp.Services;
@@ -7,17 +8,20 @@ public class ContactService : IContactService
 {
     private List<Contact> _contacts = new List<Contact>();
     private int _nextID = 0;
+    private readonly FileService _fileService = new FileService();
 
     //How to create a new contact
     public void CreateContact(Contact contact)
     {
         contact.Id = ++_nextID;
         _contacts.Add(contact);
+        _fileService.SaveListToFile(_contacts);
     }
 
     // How to list all contacts
     public IEnumerable<Contact> GetAllContacts()
     {
+        _contacts = _fileService.LoadListFromFile();
         return _contacts;
     }
 
